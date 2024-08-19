285e33eb487f471d0ba441b47d5ada5c0db3192c
Prajwal Athare <151899432+Praj1703@users.noreply.github.com>
2024-08-19 12:05:31 +0530

    Add files via upload

---
+++ ass_4.cpp
@@ -0,0 +1,41 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char data[100];
+
+    ofstream outfile;
+    outfile.open("data1.txt");
+    
+    cout << "Writing to the file ....." << endl;
+
+    cout << "Enter Your name:" << endl;
+    cin.getline(data, 100);
+    
+    outfile << data << endl;
+
+    cout << "Enter Your age:" << endl;
+    cin >> data;
+    cin.ignore();
+    
+    outfile << data << endl;
+
+    outfile.close();
+
+    ifstream infile;
+    infile.open("data1.txt");
+
+    cout << "Reading from the file:" << endl;
+
+    infile >> data;
+    cout << data << endl;
+    
+    infile >> data;
+    cout << data << endl;
+    infile.close();
+
+    return 0;
+}
+
